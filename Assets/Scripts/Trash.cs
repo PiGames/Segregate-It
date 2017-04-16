d36f1f0afa4b69a2dcfaca3b2424e650267070a5
mvxxx <yohama8832@o2.pl>
2017-04-16 22:18:41 +0200

    Folder tree, basic Trash class with prefab

---
+++ Assets/Scripts/Trash.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public Trash(Vector2 InitialVelocity)
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = InitialVelocity;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
