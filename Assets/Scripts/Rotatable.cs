b36b5d18026a49d1af3cfe6f6942208bf016ab6a
mvxxx <yohama8832@o2.pl>
2017-06-02 21:02:59 +0200

    Added rotatable script && added some trash cans

---
+++ Assets/Scripts/Rotatable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotatable : MonoBehaviour
+{
+
+    public float rotateX;
+    public float rotateY;
+    public float rotateZ;
+
+    private Transform parentTransform;
+    private Vector3 rotateVec;
+
+    // Use this for initialization
+    void Start()
+    {
+        parentTransform = GetComponentInParent<Transform>();
+        rotateVec = new Vector3(rotateX, rotateY, Mathf.Pow(-1,Random.Range(1,2))*rotateZ-Random.Range(0,2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        parentTransform.Rotate(rotateVec);
+    }
+}
