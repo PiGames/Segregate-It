5b91d1ae83e7756dc16a60ff4b0e2d1cef42fb08
mvxxx <yohama8832@o2.pl>
2017-06-03 16:06:18 +0200

    Counting  && UI

--- Assets/Scripts/TrashCan.cs
+++ Assets/Scripts/TrashCan.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class TrashCan : MonoBehaviour
 {
     public int JunkNumber = 0;
     public Junk.Type TrashCanType;
+
 
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -16,9 +18,67 @@ public class TrashCan : MonoBehaviour
         else
         {
             JunkNumber++;
+            updateUI();
         }
 
         Destroy(collider.gameObject);
 
+    }
+
+    private void updateUI()
+    {
+        switch (TrashCanType)
+        {
+            case Junk.Type.Aluminum:
+                {
+                    var textComponent = GameObject.FindGameObjectWithTag("AluminumUI")
+                        .GetComponentInChildren<UnityEngine.UI.Text>()
+                        .text;
+
+                    GameObject.FindGameObjectWithTag("AluminumUI")
+                        .GetComponentInChildren<UnityEngine.UI.Text>()
+                        .text = (Int32.Parse(textComponent) + 1).ToString();
+
+                    break;
+                }
+
+            case Junk.Type.Glass:
+                {
+                    var textComponent = GameObject.FindGameObjectWithTag("GlassUI")
+                        .GetComponentInChildren<UnityEngine.UI.Text>()
+                        .text;
+
+                    GameObject.FindGameObjectWithTag("GlassUI")
+                        .GetComponentInChildren<UnityEngine.UI.Text>()
+                        .text = (Int32.Parse(textComponent) + 1).ToString();
+
+                    break;
+                }
+
+            case Junk.Type.Paper:
+                {
+                    var textComponent = GameObject.FindGameObjectWithTag("PaperUI")
+                        .GetComponentInChildren<UnityEngine.UI.Text>()
+                        .text;
+
+                    GameObject.FindGameObjectWithTag("PaperUI")
+                        .GetComponentInChildren<UnityEngine.UI.Text>()
+                        .text = (Int32.Parse(textComponent) + 1).ToString();
+
+                    break;
+                }
+            case Junk.Type.Plastic:
+                {
+                    var textComponent = GameObject.FindGameObjectWithTag("PlasticUI")
+                        .GetComponentInChildren<UnityEngine.UI.Text>()
+                        .text;
+
+                    GameObject.FindGameObjectWithTag("PlasticUI")
+                        .GetComponentInChildren<UnityEngine.UI.Text>()
+                        .text = (Int32.Parse(textComponent) + 1).ToString();
+
+                    break;
+                }
+        }
     }
 }
